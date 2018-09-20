14bf18b10c9daeac34390eeb50cea6045c5258ed
mbbartnik <mbbartnik@gmail.com>
2018-09-20 20:09:23 +0200

    d

--- weather.py
+++ weather.py
@@ -71,7 +71,7 @@ def processRequest(req):
             humidity = str(w.get_humidity())
 
             # Get atmospheric pressure
-            pressure = str(w.get_pressure('press'))
+            pressure = int(w.get_pressure('press'))
 
             #fc = owm.daily_forecast(city)
             #f = fc.get_forecast()
