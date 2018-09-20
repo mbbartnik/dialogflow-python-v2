def6baca8933e42ed576d565bff7d01785f8dbec
mbbartnik <mbbartnik@gmail.com>
2018-09-20 18:02:38 +0200

    new

--- weather.py
+++ weather.py
@@ -12,12 +12,9 @@ owm = pyowm.OWM(owmapikey)
 @app.route('/webhook', methods=['POST'])
 def webhook():
     req = request.get_json(silent=True, force=True)
-
     print("Request:")
     print(json.dumps(req, indent=4))
-    
     res = processRequest(req)
-
     res = json.dumps(res, indent=4)
     print(res)
     r = make_response(res)
@@ -26,49 +23,63 @@ def webhook():
 
 #processing the request from dialogflow
 def processRequest(req):
-    
+
+    # taking data from dialogflow
     result = req.get("queryResult")
     parameters = result.get("parameters")
+
+    # taking parameters data
     city = parameters.get("geo-city")
     date = str(parameters.get("date"))
+    time = str(parameters.get("time"))
     name = str(parameters.get("given-name"))
     status = str(parameters.get("Status"))
     home = str(parameters.get("Home"))
+
+    # taking intent name
     meta = result.get("intent")
     intent = meta.get("displayName")
+
+    # case for intent "weather"
     if intent == "weather":
-        if city != "":
-            observation = owm.weather_at_place(city)
-        else:
-            observation = owm.weather_at_station
+        observation = owm.weather_at_place(city)
         w = observation.get_weather()
+
+        # cordinate of location
         latlon_res = observation.get_location()
-        lat=str(latlon_res.get_lat())
-        lon=str(latlon_res.get_lon())
+        lat = str(latlon_res.get_lat())
+        lon = str(latlon_res.get_lon())
 
         sun = w.get_sunset_time('iso')
-     
+
+        # wind data
         wind_res = w.get_wind()
         wind_speed = str(wind_res.get('speed'))
 
+        # cloud data
         cloud_result = str(w.get_clouds())
 
-        inf_info = str(w.get_status())
+        # weather short status
+        info_short = str(w.get_status())
 
-        humidity=str(w.get_humidity())
+        # weather detailed status
+        info_detail = str(w.get_detailed_status())
 
-        celsius_result=w.get_temperature('celsius')
-        temp_celsius=str(celsius_result.get('temp'))
+        # humidity percentage
+        humidity = str(w.get_humidity())
 
-        fahrenheit_result=w.get_temperature('fahrenheit')
-        temp_min_fahrenheit=str(fahrenheit_result.get('temp_min'))
-        temp_max_fahrenheit=str(fahrenheit_result.get('temp_max'))
+        # temperature in Celsius
+        celsius_result = w.get_temperature('celsius')
+        temp_celsius = str(celsius_result)
+        temp_min=str(celsius_result.get('temp_min'))
+        temp_max=str(celsius_result.get('temp_max'))
 
         if city != "":
-            speech = "In " + city + " we have " + temp_celsius + " °C." + "The sky is " + inf_info
+            speech = "In " + city + " we have " + temp_celsius + " °C." + "The sky is " + info_short
         else:
-            speech = "The weather for "+date+"is"
+            speech = "Please tell me which city you mean, it is necessary for proper work"
 
+    # case for intent "name"
     if intent == "name":
         if name == "Michael":
             speech = "Hello Michael, today we will talk about sience!"
@@ -82,6 +93,7 @@ def processRequest(req):
             namedropping = name
             speech = "Nice to meet you "+namedropping
 
+    # case for intent "LightHome"
     if intent == "LightsHome":
         if name == "Michael":
             speech = "Hello Michael, today we will talk about sience!"
