857aebf74d9c7e91091e6f1974d3c72fa9597973
mbbartnik <mbbartnik@gmail.com>
2018-09-23 15:14:09 +0200

    r

--- weather.py
+++ weather.py
@@ -72,7 +72,8 @@ def processRequest(req):
     # taking parameters data
     city = parameters.get("geo-city")
     date = parameters.get("date")
-    date_test = str(parameters.get("date.original"))
+    date_context = result.get("outputContexts")
+    date_test = str(date_context.get("date.original"))
     time = parameters.get("time")
     date_pariod = parameters.get("date-period")
 
